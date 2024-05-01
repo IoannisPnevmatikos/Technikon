ba8199014784a6043b6297f2a112c11aaa175c8f
andreasc <andreas.chrysolouris@gmail.com>
2024-05-01 16:59:19 +0300

    Property Service Impl & Property Repository Queries Corrections

--- TechnikonProject/src/main/java/com/team1/technikon/repository/PropertyRepository.java
+++ TechnikonProject/src/main/java/com/team1/technikon/repository/PropertyRepository.java
@@ -6,6 +6,7 @@ import com.team1.technikon.model.enums.TypeOfProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,39 +16,39 @@ import java.util.List;
 public interface PropertyRepository extends JpaRepository<Property, Long> {
 
     @Query("select p from Property p where p.owner = :tinNumber")
-    List<Property> getPropertyByOwnerTinNumber(long tinNumber);
+    List<Property> getPropertyByOwnerTinNumber(@Param("tinNumber") long tinNumber);
 
     @Query("select p from Property p where p.mapLocation =:mapLocation")
-    List<Property> getPropertyByLocation();
+    List<Property> getPropertyByLocation(); // NA TO DOYME META
 
     @Transactional
     @Modifying
     @Query("update Property p set p.propertyId = :newPropertyId where p.propertyId = :currentPropertyId")
-    int updatePropertyId(long currentPropertyId, long newPropertyId);
+    int updatePropertyId(@Param("currentPropertyId") long currentPropertyId, @Param("newPropertyId") long newPropertyId);
 
     @Transactional
     @Modifying
     @Query("update Property p set p.address = :address where p.propertyId = :propertyId")
-    int updateAddress(long propertyId, String address);
+    int updateAddress(@Param("propertyId") long propertyId, @Param("address") String address);
 
     @Transactional
     @Modifying
     @Query("update Property p set p.yearOfConstruction = :yearOfConstruction where p.propertyId = :propertyId")
-    int updateYearOfConstruction(long propertyId, String yearOfConstruction);
+    int updateYearOfConstruction(@Param("propertyId") long propertyId, @Param("yearOfConstruction") String yearOfConstruction);
 
     @Transactional
     @Modifying
     @Query("update Property p set p.typeOfProperty = :typeOfProperty where p.propertyId = :propertyId")
-    int updatePropertyType(long propertyId, TypeOfProperty typeOfProperty);
+    int updatePropertyType(@Param("propertyId") long propertyId, @Param("typeOfProperty") TypeOfProperty typeOfProperty);
 
     @Transactional
     @Modifying
     @Query("update Property p set p.photo = :photo where p.propertyId = :propertyId")
-    int updatePhoto(long propertyId, String photo);
+    int updatePhoto(@Param("propertyId") long propertyId, @Param("photo") String photo);
 
     @Transactional
     @Modifying
     @Query("update Property p set p.mapLocation = :mapLocation where p.propertyId = :propertyId")
-    int updateMapLocation(long propertyId, MapLocation mapLocation);
+    int updateMapLocation(@Param("propertyId") long propertyId, @Param("mapLocation") MapLocation mapLocation);
 
 }
