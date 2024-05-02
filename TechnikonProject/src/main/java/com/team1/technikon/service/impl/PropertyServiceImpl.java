381eeb3b9e9b2a4892b3c444d30323e427a4a845
andreasc <andreas.chrysolouris@gmail.com>
2024-05-02 11:14:47 +0300

    Check for deactivated acounts or wrong id's for properties and owners

--- TechnikonProject/src/main/java/com/team1/technikon/service/impl/PropertyServiceImpl.java
+++ TechnikonProject/src/main/java/com/team1/technikon/service/impl/PropertyServiceImpl.java
@@ -9,66 +9,117 @@ import com.team1.technikon.service.PropertyService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class PropertyServiceImpl implements PropertyService {
 
-    private PropertyRepository propertyRepository;
+    private final PropertyRepository propertyRepository;
 
     @Override
     public Property createProperty(PropertyDto propertyDto) {
-        return null;
+        if (propertyRepository.findById(propertyDto.propertyId()).orElseThrow(null) != null) return null;
+        Property property = new Property();
+        property.setPropertyId(propertyDto.propertyId());
+        property.setAddress(propertyDto.address());
+        property.setYearOfConstruction(propertyDto.yearOfConstruction());
+        property.setTypeOfProperty(propertyDto.typeOfProperty());
+        property.setPhoto(propertyDto.photo());
+        property.setMapLocation(propertyDto.mapLocation());
+        property.setActive(true);
+        property.setOwner(propertyDto.owner()); // NA TO DOYME KAI META
+        propertyRepository.save(property);
+        return property;
     }
 
     @Override
     public Property getPropertyById(long propertyId) {
-        return null;
+        Property property = propertyRepository.findById(propertyId).orElseThrow(null);
+        if (property == null) return null;
+        if (!property.isActive()) return null;
+        return property;
     }
 
     @Override
     public List<Property> getPropertyByOwnerTinNumber(long tinNumber) {
-        return List.of();
+        List<Property> properties = propertyRepository.getPropertyByOwnerTinNumber(tinNumber);
+        List<Property> response = new ArrayList<>();
+        properties.forEach(property -> {
+            if (property.isActive()) response.add(property);
+        });
+        return response;
     }
 
     @Override
     public List<Property> getPropertyByLocation() {
-        return List.of();
+        List<Property> properties = propertyRepository.getPropertyByLocation();
+        List<Property> response = new ArrayList<>();
+        properties.forEach(property -> {
+            if (property.isActive()) response.add(property);
+        });
+        return response;
     }
 
     @Override
     public boolean updatePropertyId(long currentPropertyId, long newPropertyId) {
-        return false;
+        Property property = propertyRepository.findById(currentPropertyId).orElseThrow(null);
+        if (property == null) return false;
+        if (!property.isActive()) return false;
+        return propertyRepository.updatePropertyId(currentPropertyId, newPropertyId) == 1;
     }
 
     @Override
     public boolean updateAddress(long propertyId, String address) {
-        return false;
+        Property property = propertyRepository.findById(propertyId).orElseThrow(null);
+        if (property == null) return false;
+        if (!property.isActive()) return false;
+        return propertyRepository.updateAddress(propertyId, address) == 1;
     }
 
     @Override
     public boolean updateYearOfConstruction(long propertyId, String yearOfConstruction) {
-        return false;
+        Property property = propertyRepository.findById(propertyId).orElseThrow(null);
+        if (property == null) return false;
+        if (!property.isActive()) return false;
+        return propertyRepository.updateYearOfConstruction(propertyId, yearOfConstruction) == 1;
     }
 
     @Override
     public boolean updatePropertyType(long propertyId, TypeOfProperty typeOfProperty) {
-        return false;
+        Property property = propertyRepository.findById(propertyId).orElseThrow(null);
+        if (property == null) return false;
+        if (!property.isActive()) return false;
+        return propertyRepository.updatePropertyType(propertyId, typeOfProperty) == 1;
     }
 
     @Override
     public boolean updatePhoto(long propertyId, String photo) {
-        return false;
+        Property property = propertyRepository.findById(propertyId).orElseThrow(null);
+        if (property == null) return false;
+        if (!property.isActive()) return false;
+        return propertyRepository.updatePhoto(propertyId, photo) == 1;
     }
 
     @Override
     public boolean updateMapLocation(long propertyId, MapLocation mapLocation) {
-        return false;
+        Property property = propertyRepository.findById(propertyId).orElseThrow(null);
+        if (property == null) return false;
+        if (!property.isActive()) return false;
+        return propertyRepository.updateMapLocation(propertyId, mapLocation) == 1;
     }
 
     @Override
     public boolean deleteProperty(long propertyId) {
-        return false;
+        Property property = propertyRepository.findById(propertyId).orElseThrow(null);
+        if (property == null) return false;
+        if (property.getRepairs().isEmpty()) {
+            propertyRepository.deleteById(propertyId);
+        } else {
+            property.setActive(false);
+            propertyRepository.save(property);
+        }
+        return true;
     }
 }
