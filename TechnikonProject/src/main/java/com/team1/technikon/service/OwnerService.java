fa1ec525a93b32da17e30e53658458491239a0f9
minkyeongy <minkyeong.youn@scytalys.com>
2024-05-20 13:28:42 +0300

    Fixed Repo,Service,Controller based on new Owner Schema

--- TechnikonProject/src/main/java/com/team1/technikon/service/OwnerService.java
+++ TechnikonProject/src/main/java/com/team1/technikon/service/OwnerService.java
@@ -13,15 +13,23 @@ public interface OwnerService {
     //SEARCH
     OwnerDto getOwnerByTin(String tinNumber) throws OwnerNotFoundException;
 
+    OwnerDto getOwnerByEmail(String email) throws OwnerNotFoundException;
+
+    OwnerDto getOwnerByFirstName(String firstName) throws OwnerNotFoundException;
+
+    OwnerDto getOwnerByLastName(String lastName) throws OwnerNotFoundException;
+    // GET ALL DATA
+    List<OwnerDto> getAllOwners() throws OwnerNotFoundException;
+
+    List<OwnerDto> getAllActiveOwners() throws OwnerNotFoundException;
+
     //UPDATE
     boolean updateAddress(String tinNumber, String address) throws OwnerNotFoundException;
 
+    boolean updateOwnerByPhone(String tinNumber, String phoneNumber) throws OwnerNotFoundException;
 
     //DELETE
     boolean deleteOwner(String tinNumber) throws OwnerNotFoundException;
 
-    // GET ALL DATA
-    List<OwnerDto> getAllData() throws OwnerNotFoundException;
 
-//    List<OwnerDto> getAllActiveOwners() throws OwnerNotFoundException;
 }
