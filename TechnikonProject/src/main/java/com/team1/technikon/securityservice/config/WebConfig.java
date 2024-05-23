1e30794b4297627099a9703fb26874c6438cf895
andreasc <andreas.chrysolouris@gmail.com>
2024-05-23 22:42:14 +0300

    testing something

--- TechnikonProject/src/main/java/com/team1/technikon/securityservice/config/WebConfig.java
+++ TechnikonProject/src/main/java/com/team1/technikon/securityservice/config/WebConfig.java
@@ -1,2 +1,17 @@
-package com.team1.technikon.securityservice.config;public class WebConfig {
+package com.team1.technikon.securityservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Define the path pattern for which CORS should be enabled
+                .allowedOrigins("http://localhost:5173") // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+    }
 }
+
